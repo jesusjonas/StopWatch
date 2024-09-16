2dbbe406ab7569129ac849f6db3ca596a49bc97c
jesusjonas <jonas-oliveira25@hotmail.com>
2024-09-16 19:11:11 -0300

    Inicio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    static void Start()
+    {
+        int time = 10;
+
+
+    }
+
+}
